9d1b127dca52316dfaa122dfa9cf011d6ac15deb
Lê Cảnh Toàn <lcnhton@gmail.com>
2023-12-16 10:10:53 +0700

    Update ferriswheel.cpp

--- Sorting and Searching/ferriswheel.cpp
+++ Sorting and Searching/ferriswheel.cpp
@@ -5,15 +5,14 @@ using namespace std;
 int a[200005];
 
 int main() {
-    int n,x;
-    cin>>n>>x;
-    for (int i=0;i<n;i++)
-        cin>>a[i];
-    sort(a,a+n);
-    int i=0,j=n-1;
-    int res=0;
-    while (i<j) {
-        if (a[i]+a[j]<=x) {
+    int n, x;
+    cin >> n >> x;
+    for (int i = 0; i < n; i++) cin >> a[i];
+    sort(a, a + n);
+    int i = 0, j = n - 1;
+    int res = 0;
+    while (i < j) {
+        if (a[i] + a[j] <= x) {
             res++;
             i++;
             j--;
@@ -22,5 +21,5 @@ int main() {
             j--;
         }
     }
-    cout<<res+(i==j);
+    cout << res + (i == j);
 }
